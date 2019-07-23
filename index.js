cbd81d3799ff4b5525432210362c3da79254fd0c
Rafael <rafael.acerqueirah@gmail.com>
2019-07-23 11:42:19 -0300

    Create middlewares and use it in consign

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const app = require("express")();
+const db = require("./config/db");
+const consign = require("consign");
 
-app.get("/", (req, res) => {
-  res.status(200).send("nheee");
-});
+consign()
+  .then("./config/middlewares.js")
+  .into(app);
+
+app.db = db;
 
 app.listen(3000, (req, res) => {
   console.log("BACKEND RUNNING");
